f396b4ce60f22975ad25292babbc5f171364f426
Zoenamed <1402677114@qq.com>
2024-01-09 12:38:18 +0800

    almost finish 1.datalab

--- csapplab/datalab/datalab-handout/bits.c
+++ csapplab/datalab/datalab-handout/bits.c
@@ -1,7 +1,7 @@
 /* 
  * CS:APP Data Lab 
  * 
- * <Please put your name and userid here>
+ * <2024.01.09: 28/36 = 77分>
  * 
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
@@ -69,6 +69,15 @@ INTEGER CODING RULES:
   7. Use any data type other than int.  This implies that you
      cannot use arrays, structs, or unions.
 
+  您被明确禁止：
+
+  1. 使用任何控制结构，如 if、do、while、for、switch 等。
+  2. 定义或使用任何宏。
+  3. 在此文件中定义任何额外的函数。
+  4. 调用任何函数。
+  5. 使用任何其他操作，例如 &&、||、- 或 ?。
+  6. 使用任何形式的类型转换。
+  7. 使用除 int 以外的任何数据类型。这意味着您不能使用数组、结构体或联合。
  
   You may assume that your machine:
   1. Uses 2s complement, 32-bit representations of integers.
@@ -76,6 +85,10 @@ INTEGER CODING RULES:
   3. Has unpredictable behavior when shifting if the shift amount
      is less than 0 or greater than 31.
 
+  您可以假设您的计算机：
+  1. 使用二进制补码，32位整数的表示形式。
+  2. 在算术右移时执行右移操作。
+  3. 在进行移位操作时，如果移位量小于0或大于31，则具有不可预测的行为。
 
 EXAMPLES OF ACCEPTABLE CODING STYLE:
   /*
@@ -95,6 +108,27 @@ EXAMPLES OF ACCEPTABLE CODING STYLE:
      result += 4;
      return result;
   }
+
+可接受的编码样式示例：
+
+  /*
+  * pow2plus1 - 返回 2^x + 1，其中 0 <= x <= 31
+  */
+  int pow2plus1(int x) {
+      /* 利用移位操作计算2的幂 */
+      return (1 << x) + 1;
+  }
+
+  /*
+  * pow2plus4 - 返回 2^x + 4，其中 0 <= x <= 31
+  */
+  int pow2plus4(int x) {
+      /* 利用移位操作计算2的幂 */
+      int result = (1 << x);
+      result += 4;
+      return result;
+  }
+
 
 FLOATING POINT CODING RULES
 
@@ -150,7 +184,9 @@ NOTES:
  *   Rating: 1
  */
 int bitXor(int x, int y) {
-  return 2;
+  // x^y = (x & ~y) | (~x & y)
+  // 画图分析
+  return ~(x & y)&~(~x & ~y);
 }
 /* 
  * tmin - return minimum two's complement integer 
@@ -159,9 +195,10 @@ int bitXor(int x, int y) {
  *   Rating: 1
  */
 int tmin(void) {
-
-  return 2;
-
+  // 返回最小的补码整数，32位机器-> -2^31
+  // 参考pow2plus1
+  int x = 31;
+  return 1<<x;
 }
 //2
 /*
@@ -172,7 +209,11 @@ int tmin(void) {
  *   Rating: 1
  */
 int isTmax(int x) {
-  return 2;
+  // isTmax - 如果 x 是最大的二进制补码整数，则返回1，否则返回0
+  // Tmax + Tmin = 0xFFFFFFFF (二进制全1)
+  // ?如何判断大小。在不调用条件语句的情况下
+  int TMIN = tmin();
+  return !~(x+TMIN);
 }
 /* 
  * allOddBits - return 1 if all odd-numbered bits in word set to 1
@@ -183,7 +224,12 @@ int isTmax(int x) {
  *   Rating: 2
  */
 int allOddBits(int x) {
-  return 2;
+  // allOddBits - 如果 word 的所有奇数位都设置为 1，则返回 1(0~31位有效)
+  /*
+   *  所有奇数位为1，偶数位为0的数是：0xAAAAAAAA
+   */
+  x = x&0xAAAAAAAA;
+  return !(x^0xAAAAAAAA);
 }
 /* 
  * negate - return -x 
@@ -193,7 +239,8 @@ int allOddBits(int x) {
  *   Rating: 2
  */
 int negate(int x) {
-  return 2;
+  // 考加法逆元，补码的非：按位取反+1
+  return ~x + 1;
 }
 //3
 /* 
@@ -206,7 +253,12 @@ int negate(int x) {
  *   Rating: 3
  */
 int isAsciiDigit(int x) {
-  return 2;
+  // 判断该数是否在0x30~0x39之间
+  // 1. 大于等于0x30， x-0x30不为负数，看符号位
+  // 2. 小于等于0x39， 0x39-x不为负数
+  int first = !((negate(0x30) + x)>>31);
+  int second = !((0x39 + negate(x))>>31);
+  return first&second;
 }
 /* 
  * conditional - same as x ? y : z 
@@ -216,7 +268,10 @@ int isAsciiDigit(int x) {
  *   Rating: 3
  */
 int conditional(int x, int y, int z) {
-  return 2;
+  // x!=0 ? y:z
+  // (1)的补码 全1， (0）的补码为全0
+  int ans = negate(!!x); // 将x转为bool值,扩展为全1或全0
+  return (ans & y) | (~ans & z);
 }
 /* 
  * isLessOrEqual - if x <= y  then return 1, else return 0 
@@ -226,7 +281,10 @@ int conditional(int x, int y, int z) {
  *   Rating: 3
  */
 int isLessOrEqual(int x, int y) {
-  return 2;
+  // 结合isAsciiDigit函数
+  // 判断x、y的大小
+  int flag = !((negate(x) + y)>>31);
+  return flag;
 }
 //4
 /* 
@@ -238,7 +296,11 @@ int isLessOrEqual(int x, int y) {
  *   Rating: 4 
  */
 int logicalNeg(int x) {
-  return 2;
+  // 逻辑非：只有0x0的情况才是1，不然返回0
+  // 判断x!=0
+  // +0 == -0, 将对整个数的判断转换为对符号位的判断
+  // (negate(x) | x)>>31 + 1
+  return (((~x + 1) | x)>>31) + 1;
 }
 /* howManyBits - return the minimum number of bits required to represent x in
  *             two's complement
@@ -253,7 +315,35 @@ int logicalNeg(int x) {
  *  Rating: 4
  */
 int howManyBits(int x) {
-  return 0;
+  // 返回在二进制补码中表示 x 所需的最小位数——二分法
+  // 1. 判断正负，
+  //int flag = ~(!!(x>>31))+1;//判断正负并拓展位
+  int flag = ~(!!(x>>31))+1; // 将x符号位转为bool值,扩展为全1或全0
+  x =  (flag & ~x) | (~flag & x);
+
+  // 2. 判断16位
+  int b16 = !!(x>>16);//求高16位是否有值
+  x = x >> (b16<<4); // 有就丢弃低16位
+
+  // 2. 判断8位
+  int b8 = !!(x>>8);
+  x = x >> (b8<<3);
+
+  // 3. 判断4位
+  int b4 = !!(x>>4);
+  x = x >> (b4<<2);
+
+  // 4. 判断2位
+  int b2 = !!(x>>2);
+  x = x >> (b2<<1);
+
+  // 5. 判断1位
+  int b1 = !!(x>>1);
+  x = x >> b1;
+  
+  int b0 = !!x;
+  //
+  return (b16<<4)+(b8<<3)+(b4<<2)+(b2<<1)+b1+b0+1;
 }
 //float
 /* 
@@ -267,8 +357,34 @@ int howManyBits(int x) {
  *   Max ops: 30
  *   Rating: 4
  */
+/*
+ * floatScale2 - 返回浮点数表达式 2*f 的位级等效表示
+ *   输入和输出都以无符号整数（unsigned int）的形式表示，
+ *   但应该将它们解释为单精度浮点数的位级表示。
+ * 参数:
+ *   uf: 以无符号整数表示的单精度浮点数
+ * 返回:
+ *   以无符号整数表示的单精度浮点数，表示 2*f 的位级等效表示
+ *   当输入 uf 表示的浮点数是 NaN 时，返回原始输入 uf
+ * 约束:
+ *   可以使用整数和无符号整数的任意操作，包括 ||、&&、if、while
+ *   最大操作数（ops）数为30
+ * 难度: 4
+ */
 unsigned floatScale2(unsigned uf) {
-  return 2;
+  // 32位浮点数：1S+8exp+23frac
+  // 2*f 等价于 2.0xf
+  // 
+  // 1. NaN : exp阶码为全1
+  unsigned sign = uf & (1<<31);
+  unsigned exp = (uf & 0x7f800000)>>23;
+  unsigned frac = uf & (~0xff800000);
+  if (exp == 0xff) return uf; // exp = 11111111, NaN
+  if (exp == 0) return sign | frac << 1;
+  // 规格化
+  exp++;
+  if (exp == 0xff) return sign | 0x7f800000; // 溢出，返回+inf/-inf
+  return sign | (exp << 23) | frac;
 }
 /* 
  * floatFloat2Int - Return bit-level equivalent of expression (int) f
@@ -282,7 +398,23 @@ unsigned floatScale2(unsigned uf) {
  *   Max ops: 30
  *   Rating: 4
  */
+/* 
+ * floatFloat2Int - 返回浮点数参数 f 的表达式 (int) f 的位级等效表示。
+ *   浮点数参数 f 以无符号整数形式传递，但
+ *   应该将其解释为单精度浮点数的位级表示。
+ *   任何超出范围的值（包括 NaN 和无穷大）都应返回
+ *   0x80000000u。
+ *   合法操作：任何整数/无符号操作，包括 ||、&&。还有 if、while
+ *   最大操作数：30
+ *   评分：4
+ */
 int floatFloat2Int(unsigned uf) {
+  /* !!! 舍入
+   * double/float → int
+   * § Truncates fractional part
+   * § Like rounding toward zero
+   * § Not defined when out of range or NaN: Generally sets to TMin
+   */
   return 2;
 }
 /* 
